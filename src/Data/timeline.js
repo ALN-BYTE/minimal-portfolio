75bc2e9e4f28e91bee2d43155538ee899c0d0925
XLbombie <119650443+ALN-BYTE@users.noreply.github.com>
2023-09-12 21:51:25 -0500

    updated timeline files

--- src/Data/timeline.js
+++ src/Data/timeline.js
@@ -4,13 +4,13 @@ export default [
         title: "Youtube Content Creator",
         duration: "6 years",
         details: " Description of the YouTube Content Creator"
-    }
+    },
     {
         year: "2020",
         title: "School",
         duration: "6 years",
         details: " Description of the YouTube Content Creator"
-    }
+    },
     {
         year: "2019",
         title: "Gap Year",
