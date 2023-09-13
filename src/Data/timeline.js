f3ad0dfc8d24403558c8c5f1c470460684c23a58
XLbombie <119650443+ALN-BYTE@users.noreply.github.com>
2023-09-12 21:36:57 -0500

    Added array of objects for timeline

---
+++ src/Data/timeline.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        year: "Today",
+        title: "Youtube Content Creator",
+        duration: "6 years",
+        details: " Description of the YouTube Content Creator"
+    }
+    {
+        year: "2020",
+        title: "School",
+        duration: "6 years",
+        details: " Description of the YouTube Content Creator"
+    }
+    {
+        year: "2019",
+        title: "Gap Year",
+        duration: "6 years",
+        details: " Description of the YouTube Content Creator"
+    }
+]
